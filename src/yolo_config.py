a352ab4d57bba335e03116c57c25404727df4d09
erikajoun <erikajoun@gmail.com>
2020-10-03 19:41:15 -0700

    Update yolo_config.py

--- src/yolo_config.py
+++ src/yolo_config.py
@@ -4,7 +4,7 @@ Configurations for YOLO model.
 
 MODEL_PATH = "yolo-coco"
 NMS_THRESH = 0.3
-USE_GPU = False  # NVIDIA GPU usage for inference
+USE_GPU = True  # NVIDIA GPU usage for inference
 
 min_conf = [0.9]  # minimum confidence level
 min_distance = [50]  # minimum social distancing distance
